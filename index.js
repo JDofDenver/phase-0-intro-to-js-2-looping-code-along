f94034c9ec92d233a6623c38ce843f695eb6e77b
John <johndawson.inbox@gmail.com>
2022-12-13 23:07:08 -0700

    commit tree 6808f6220af8d6327c5cd9f02c3e7238c1a9654a

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const newArray =[]
+function writeCards(name, event){
+    for (let i = [0]; i< name.length; i++){
+        
+        newArray.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+
+    }
+    return newArray;
+}
+//writeCards(["Charlie", "Samip", "Ali"], "birthday");
+//console.log(newArray);
+
+function countDown(int){
+    while (int >= 0){
+        console.log(int--);
+    }
+}
+
+
